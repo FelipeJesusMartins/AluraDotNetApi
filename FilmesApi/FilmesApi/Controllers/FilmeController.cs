e37a868b21aa762ffb92abfb5e6c8dfd6ee08269
FelipeJesusMartins <felipemartins1359@gmail.com>
2023-01-18 16:48:55 -0300

    First commit

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AdcionaFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmesPorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmesPorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        return filme == null ? NotFound() : Ok();  
+    }
+
+}
